7c16c848d875f5f9b20076fb8477722362e80499
kimbomm <qhals0710@gmail.com>
2017-06-29 13:53:03 +0900

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+$(function() {
+  var body_Width, barLeft, li_W;
+  var body_move = false;
+
+  $(window).resize(function() {
+    body_Width = $('body,html').width();
+    li_W = $('.top_menu>li').width();
+    barLeft = $('.top_menu li:first-child').offset().left - li_W;
+  })
+  $(window).trigger('resize');
+
+  //메인메뉴
+  $('.bars').css({
+    'left': 'barLeft',
+    'display': 'block'
+  });
+  $('.top_menu > li').mouseenter(function() {
+    $('.bars').stop().css('display', 'block').animate({
+      'left': barLeft + (li_W * ($(this).index() + 1))
+    })
+    $('.top_menu div').stop().slideDown();
+    $('header').stop().animate({
+      'height': 250
+    })
+  }).mouseleave(function() {
+    $('.bars').stop().animate({
+      'left': barLeft
+    }, function() {
+      $(this).css('display', 'none')
+    })
+    $('.top_menu div').stop().slideUp();
+    $('header').stop().animate({
+      'height': 80
+    })
+  })
+
+
+  $('.com_sec li').mouseenter(function() {
+    $(this).find('.bar').stop().animate({
+      'width': 0
+    });
+    if(body_Width >= 1080){
+      $(this).find('.line1').stop().animate({
+        'width': 100 + '%'
+      }, function() {
+        $(this).next().stop().animate({
+          'height': 100 + '%'
+        }, function() {
+          $(this).next().stop().animate({
+            'width': 100 + '%'
+          }, function() {
+            $(this).next().stop().animate({
+              'height': 100 + '%'
+            })
+          })
+        })
+      })
+    }
+  }).mouseleave(function() {
+    $(this).find('.bar').stop().animate({
+      'width': 20
+    });
+    if(body_Width >= 1080){
+      $(this).find('.line1' + ',' + '.line3').stop().animate({
+        'width': 0
+      });
+      $(this).find('.line2' + ',' + '.line4').stop().animate({
+        'height': 0
+      })
+    }
+  })
+
+  //카테고리 클릭
+  $('.cate').click(function() {
+    if (body_Width <= 1007) {
+      console.log('aaa');
+      if (!body_move) {
+        $('body').animate({
+          'left': -240
+        })
+        body_move = true;
+        console.log(body_move);
+      } else if (body_move) {
+        $('body').animate({
+          'left': 0
+        })
+        body_move = false;
+        console.log(body_move);
+      }
+    }
+  })
+
+
+
+})
