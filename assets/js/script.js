27e03a99abb333a9cde5c2f204af87edf6483038
kimbomm <qhals0710@gmail.com>
2017-06-30 10:14:49 +0900

    teakwang commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,41 @@
 $(function() {
   var body_Width, barLeft, li_W;
+  var scrWidth = 17;
   var body_move = false;
 
   $(window).resize(function() {
-    body_Width = $('body,html').width();
+    body_Width = $('body,html').width()+scrWidth;
     li_W = $('.top_menu>li').width();
     barLeft = $('.top_menu li:first-child').offset().left - li_W;
   })
   $(window).trigger('resize');
 
   //메인메뉴
-  $('.bars').css({
-    'left': 'barLeft',
-    'display': 'block'
-  });
-  $('.top_menu > li').mouseenter(function() {
-    $('.bars').stop().css('display', 'block').animate({
-      'left': barLeft + (li_W * ($(this).index() + 1))
+  if(body_Width > 1024){
+    $('.bars').css({
+      'left': 'barLeft',
+      'display': 'block'
+    });
+    $('.top_menu > li').mouseenter(function() {
+      $('.bars').stop().css('display', 'block').animate({
+        'left': barLeft + (li_W * ($(this).index() + 1))
+      })
+      $('.top_menu div').stop().slideDown();
+      $('header').stop().animate({
+        'height': 250
+      })
+    }).mouseleave(function() {
+      $('.bars').stop().animate({
+        'left': barLeft
+      }, function() {
+        $(this).css('display', 'none')
+      })
+      $('.top_menu div').stop().slideUp();
+      $('header').stop().animate({
+        'height': 80
+      })
     })
-    $('.top_menu div').stop().slideDown();
-    $('header').stop().animate({
-      'height': 250
-    })
-  }).mouseleave(function() {
-    $('.bars').stop().animate({
-      'left': barLeft
-    }, function() {
-      $(this).css('display', 'none')
-    })
-    $('.top_menu div').stop().slideUp();
-    $('header').stop().animate({
-      'height': 80
-    })
-  })
+  }
 
 
   $('.com_sec li').mouseenter(function() {
